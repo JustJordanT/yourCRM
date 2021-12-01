a378c0806b83518629e17b6b9343c711681756b3
JustJordanT <jordantay9014@outlook.com>
2021-12-01 16:51:36 -0600

    adding initial files

---
+++ yourCRM/projects/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class contact
+{
+    private string firstName;
+    private string lastName;
+    private string organization;
+    private int phoneNumber;
+    private string address;
+
+    public contact(string organization, string lastName, string firstName, int phoneNumber, string address)
+    {
+        this.organization = organization;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+}
